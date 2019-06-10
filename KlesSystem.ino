a4677b063858f3e0ac81a1e96f74bb498ce1eb40
kroglid <sander.kroglid@icloud.com>
2019-06-10 16:32:56 +0200

    Update KlesSystem.ino

--- KlesSystem.ino
+++ KlesSystem.ino
@@ -1,33 +1,37 @@
-const int trykkPlateKunde = 4;
-const int mottakerVibrasjon = 13;
-const int trykkPlateAnsatt = 2;
-const int mottakerKnapp = 12;
+//Definerer input til komponentene
+const int trykkPlateKunde = 4; //Input til trykkplate forran kasse
+const int mottakerVibrasjon = 13; //Input til vibrasjonsmotorene i mottakerne
+const int trykkPlateAnsatt = 2; //Input til trykkplate bak kasse
+const int mottakerKnapp = 12; //Input til knappene i mottakerne
 
+//Variabel som holder på statusen fra mottakerknappene, trykkplate forran kasse, og trykkplaten bak kasse
 int mkStatus;
 int tpkStatus;
 int tpaStatus;
+
+//Variabel som holder på forrige statusen fra mottakerknappene, trykkplate forran kasse, og trykkplaten bak kasse
 int forrigemkStatus = LOW;
 int forrigetpkStatus = LOW;
 int forrigetpaStatus = LOW;
+
+//Variabel som holder på tiden siden forrige knappetrykk
 unsigned long mkTid = 0;
 unsigned long tpkTid = 0;
 unsigned long tpaTid = 0;
-unsigned long vmTid = 0;
 
-int vibrasjonsStatus = LOW;
-int vibrasjonsVeksling = LOW;
-
+//DebounceDelay for mottakerknappene og trykkplate bak kasse
+//DebounceDelay er til for å forhinde dobbeltklikk
 unsigned long debounceDelay = 50;
 
+//DebounceDelay for trykkplaten forran kasse
 unsigned long trykkPlateKundeDelay = 3000;
 
 void setup() {
+  //Setter opp komponentene
   pinMode(trykkPlateKunde, INPUT);
   pinMode(mottakerVibrasjon, OUTPUT);
   pinMode(trykkPlateAnsatt, INPUT);
   pinMode(mottakerKnapp, INPUT);
-
-  Serial.begin(9600);
 }
 
 void loop() {
@@ -50,7 +54,6 @@ void loop() {
   forrigemkStatus = leseAMKnapp;
 
   //Debounce trykkPlateAnsatt
-
   int leseAKKnapp = digitalRead(trykkPlateAnsatt);
   
   if (leseAKKnapp != forrigetpaStatus) {
